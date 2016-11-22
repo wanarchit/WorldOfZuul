c465682a49a1d3aaf2d5de06eb5e60edca99f52e
Paul <Paul@DESKTOP-1U9DTNA>
2016-11-22 18:24:12 +0100

    Merge origin/master
    Conflicts:
    	JavaApplication2/src/javaapplication2/Character.java
    	JavaApplication2/src/javaapplication2/Chest.java
    	JavaApplication2/test/javaapplication2/ChestTest.java
    	JavaApplication2/test/javaapplication2/PlayerTest.java

--- JavaApplication2/test/javaapplication2/ItemTest.java
+++ JavaApplication2/test/javaapplication2/ItemTest.java
@@ -1,4 +1,3 @@
-
 package javaapplication2;
 
 import org.junit.After;
@@ -10,27 +9,48 @@ public class ItemTest {
     private Item item;
     private Inventory inv1;
     
+    /**
+     * The constructor for the class ItemTest.
+     */
     public ItemTest() 
     {
     }
     
+    /**
+     * Set up the fixture
+     * Called before every test case methods.
+     * Initialize the item for each test.
+     */
     @Before
     public void setUp() 
     {
        item = new Item("name1", inv1, 50);
     }
     
+    /**
+     * Tear Down the test fixture
+     * Called after every test case method
+     */
     @After
     public void tearDown() 
     {
         item = null;
     }
-
+    
+    /**
+     * Method testUse for the item.
+     * If an item is present in the inventory, the Player can use this.
+     * The Player cannot use an item absent in the inventory.
+     */
     @Test
     public void testUse() 
     {
     }
 
+    /**
+     * Method testGetPrice
+     * Checks if the getter works well, here, object was initialized to 50
+     */
     @Test
     public void testGetPrice() {
         assertEquals(50, item.getPrice());
