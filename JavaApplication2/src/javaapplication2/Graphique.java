8ee1db6b4c421adde17609f76fecb6434b20088f
Marin <Marin@192.168.24.145>
2016-12-01 18:12:34 +0100

    Merge origin/master
    Conflicts:
    	JavaApplication2/src/javaapplication2/Armor.java
    	JavaApplication2/src/javaapplication2/Chest.java
    	JavaApplication2/src/javaapplication2/Consummable.java
    	JavaApplication2/src/javaapplication2/Inventory.java

--- JavaApplication2/src/javaapplication2/Graphique.java
+++ JavaApplication2/src/javaapplication2/Graphique.java
@@ -11,9 +11,13 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -24,12 +28,24 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
 
-public class Graphique {
+public class Graphique extends JFrame implements ActionListener {
 private Room actRoom;
 private HashMap <String, Door> roomHM;
 private boolean north,south,east,west;
+private JButton doorSouth,doorNorth,doorWest,doorEast;
+private Player mPlayer;
+private JFrame frame;
+private JPanel gameGrid;
+private JButton arrayButton[];
+private JButton arrayChest[];
+private int i=0; // NCP counter
+private int c=0; // Chest counter
+private int nbButt=0;
+private ArrayList<NPC> listNPC;
 
-    public void Affiche(Player myPlayer) {
+    public JPanel Affiche(Player myPlayer) {
+        mPlayer=myPlayer;
+        listNPC=myPlayer.getActualRoom().getNPCRoom();
         //final ImageIcon backgroundImage = new ImageIcon("C:/imgWoZ/ground.jpg");
         //JLabel gameGrid = new JLabel(backgroundImage) {
         //};
@@ -39,7 +55,7 @@ private boolean north,south,east,west;
 
         //----------------------------------------------------------------------
         //-----------------------------GAME GRID--------------------------------
-        JLabel gameGrid = new JLabel();
+        gameGrid = new JPanel();
         gameGrid.setLayout(new GridLayout(5,5));
         actRoom=myPlayer.getActualRoom();
         roomHM=actRoom.getHashMap();
@@ -55,6 +71,16 @@ private boolean north,south,east,west;
         if(roomHM.containsKey("East")){
             east=true;
         }        
+
+        //----------------------------------------------------------------------
+        arrayButton = new JButton[myPlayer.getActualRoom().getNPCRoom().size()];
+        arrayChest = new JButton[myPlayer.getActualRoom().getChestRoom().size()];
+        for(int j=0; j< myPlayer.getActualRoom().getNPCRoom().size(); j++){
+            System.out.println("NPC"+j);           
+        }
+        //------------------------Adding NPC------------------------------------
+ 
+        //--------------------
         for(int row=0; row < 5; row ++){
             for(int col=0; col < 5; col ++){
         //----------------------------------------------------------------------
@@ -62,10 +88,15 @@ private boolean north,south,east,west;
                 if(south==true && row==4 && col==2)
                 {
                     ImageIcon porte = new ImageIcon(new ImageIcon("C:/imgWoz/porte1.jpg").getImage().getScaledInstance(120,160, Image.SCALE_DEFAULT));
-                    JButton button = new JButton(porte);
-                    button.setPreferredSize(new Dimension(100,100));
-                    button.setMinimumSize(new Dimension(100,100));
-                    gameGrid.add(button);
+                    doorSouth = new JButton(porte);
+                    doorSouth.setPreferredSize(new Dimension(100,100));
+                    doorSouth.setMinimumSize(new Dimension(100,100));
+                    gameGrid.add(doorSouth);
+                    myPlayer.setActualRoom(roomHM.get("South").getNextRoom());
+                    doorSouth.addActionListener(this);
+                    System.out.println(this.getComponentZOrder(doorSouth));
+                    
+                    
                 }
                 else if(north==true && row==0 && col==2)
                 {
@@ -82,6 +113,7 @@ private boolean north,south,east,west;
                     button.setPreferredSize(new Dimension(100,100));
                     button.setMinimumSize(new Dimension(100,100));
                     gameGrid.add(button);
+                    //System.out.println(doorWest.getX()+" "+doorWest.getY());
                 }
                 else if(east==true && row==2 && col==4)
                 {
@@ -90,79 +122,67 @@ private boolean north,south,east,west;
                     button.setPreferredSize(new Dimension(100,100));
                     button.setMinimumSize(new Dimension(100,100));
                     gameGrid.add(button);
-                }                
+                }
+                else if( i < arrayButton.length && nbButt!=2 && nbButt!=10 && nbButt!=14 && nbButt!=22 ){
+                    if(listNPC.get(i).isHostile()){
+                        ImageIcon host = new ImageIcon(new ImageIcon("C:/imgWoz/monstre1.png").getImage().getScaledInstance(120,160, Image.SCALE_DEFAULT));
+                        arrayButton[i]= new JButton(host);
+                        arrayButton[i].setPreferredSize(new Dimension(100,100));
+                        arrayButton[i].setMinimumSize(new Dimension(100,100));
+                        gameGrid.add(arrayButton[i]);
+                        arrayButton[i].addActionListener(this);
+                        i++;
+                    }
+                    else{
+                        ImageIcon friendly = new ImageIcon(new ImageIcon("C:/imgWoz/friendly.jpg").getImage().getScaledInstance(120,160, Image.SCALE_DEFAULT));
+                        arrayButton[i] = new JButton(friendly);
+                        arrayButton[i].setPreferredSize(new Dimension(100,100));
+                        arrayButton[i].setMinimumSize(new Dimension(100,100));
+                        gameGrid.add(arrayButton[i]);
+                        arrayButton[i].addActionListener(this);
+                        i++;
+                    }
+                }
+                else if(c < arrayChest.length && nbButt!=2 && nbButt!=10 && nbButt!=14 && nbButt!=22){
+                    ImageIcon chest= new ImageIcon(new ImageIcon("C:/imgWoz/chest.png").getImage().getScaledInstance(120,160, Image.SCALE_DEFAULT));
+                    arrayChest[c]= new JButton(chest);
+                    arrayChest[c].setPreferredSize(new Dimension(100,100));
+                    arrayChest[c].setMinimumSize(new Dimension(100,100));
+                    gameGrid.add(arrayChest[c]);
+                    arrayChest[c].addActionListener(this);
+                    c++;
+                }
                 else{
-                //----------------------------------------------------------------------   
+                    
                     ImageIcon dalle1 = new ImageIcon(new ImageIcon("C:/imgWoz/dalleFinal.png").getImage().getScaledInstance(120,160, Image.SCALE_DEFAULT));
                     JButton button2 = new JButton(dalle1);
                     button2.setPreferredSize(new Dimension(100,100));
                     button2.setMinimumSize(new Dimension(100,100));
                     gameGrid.add(button2);
-                }   
+                }
+                nbButt++;
             }
+            
         }
-        
-            
-            
-        
- 
-        
         gameGrid.setPreferredSize(new Dimension(600,500));//largeur,hauteur
         gameGrid.setMinimumSize(new Dimension(600,500));
-        //----------------------------------------------------------------------
-        //--------------------------TOP PANEL-----------------------------------
-        JLabel hudTop = new JLabel();
-        hudTop.setLayout(new GridLayout(1,3));        
-        hudTop.setPreferredSize(new Dimension(700,200));
-        hudTop.setMinimumSize(new Dimension(700,200));
-        //----------------------------------------------------------------------
-        JLabel hudTopLeft = new JLabel();
-        hudTopLeft.setLayout(new GridLayout(3,1));
-        hudTopLeft.add(new JButton("hp"));
-        hudTopLeft.add(new JButton("karma"));
-        hudTopLeft.add(new JButton("armor"));
-        hudTop.add(hudTopLeft);
-        //---------------------------------------------------------------------
-        JLabel hudTopCenter = new JLabel();
-        hudTopCenter.setLayout(new GridLayout(1,1));
-        ImageIcon test = new ImageIcon(new ImageIcon("C:/imgWoz/babbu2.jpg").getImage().getScaledInstance(500,500, Image.SCALE_DEFAULT));
-        hudTopCenter.add(new JButton(test));
-        hudTop.add(hudTopCenter);
-        //----------------------------------------------------------------------
-        JLabel hudTopRight = new JLabel();
-        hudTopRight.setLayout(new GridLayout(3,1));
-        hudTopRight.add(new JButton("Inventory"));
-        hudTopRight.add(new JButton("Armor Equiped"));
-        hudTopRight.add(new JButton("Weapon Equiped"));
-        hudTop.add(hudTopRight);
-
-        //----------------------------------------------------------------------
-        //----------------------------RIGHT PANEL-------------------------------
-        JLabel hudRight = new JLabel();
-        hudRight.setLayout(new GridLayout(3,1));
-        JButton button1 = new JButton("Interaction 1");
-       // button1.setBackground(Color.RGBtoHSB(102, 51, 0, null));//102.51
-        hudRight.add(button1);
-        hudRight.add(new JButton("Interaction 2"));
-        hudRight.add(new JButton("Interaction 3"));
-        hudRight.setPreferredSize(new Dimension(100,800));
-        hudRight.setMinimumSize(new Dimension(100,800));
-        //----------------------------------------------------------------------
-        //----------------------------------------------------------------------
-        frame.add(gameGrid,BorderLayout.WEST);
-        frame.add(hudTop,BorderLayout.NORTH);
-        frame.add(hudRight,BorderLayout.EAST);
-        frame.pack();
-        frame.revalidate();
-        frame.repaint();
-        frame.setVisible(true);
-
-        
+        this.add(gameGrid,BorderLayout.WEST);
+        this.pack();
+        this.setVisible(true);
+        return gameGrid;
     }
-        //----------------------------------------------------------------------
-    
-    //--------------------------------------------------------------------------
-    //----------------------Rescaling Image to JLabel---------------------------
-    
-    
+     //--------------------------------------------------------------------------
+    //----------------------ActionListener---------------------------
+    public void actionPerformed (ActionEvent e){
+         if (e.getSource().equals(doorSouth)){
+            System.out.println("toto");
+            Graphique newAffiche = new Graphique();
+            gameGrid=newAffiche.Affiche(mPlayer);
+            this.dispose();
+            this.setContentPane(newAffiche.Affiche(mPlayer));
+                     
+        }
+
+       
+}
 }
