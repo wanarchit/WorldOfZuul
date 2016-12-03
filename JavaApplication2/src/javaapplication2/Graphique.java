4a07a73b136c3a6177bb4ba9243820624e5ca418
Marin <Marin@192.168.24.145>
2016-12-03 15:12:39 +0100

    Merge origin/master
    Conflicts:
    	JavaApplication2/src/javaapplication2/Armor.java
    	JavaApplication2/src/javaapplication2/Chest.java
    	JavaApplication2/src/javaapplication2/Consummable.java
    	JavaApplication2/src/javaapplication2/Inventory.java

--- JavaApplication2/src/javaapplication2/Graphique.java
+++ JavaApplication2/src/javaapplication2/Graphique.java
@@ -19,6 +19,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.concurrent.ThreadLocalRandom;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.Box;
@@ -38,21 +39,37 @@ private JFrame frame;
 private JPanel gameGrid;
 private JButton arrayButton[];
 private JButton arrayChest[];
+private int nbPos=0; //Number of objects to add to the gameGrid
 private int i=0; // NCP counter
 private int c=0; // Chest counter
-private int nbButt=0;
+private int cpt=0;
 private ArrayList<NPC> listNPC;
+private ArrayList<Chest> listCh;
+//private ArrayList<Integer> listHost;
+//private ArrayList<Integer> listFriend;
+//private ArrayList<Integer> listChest;
+private int min=0;
+private int max=24;
+private int rdVal;
+
 
     public JPanel Affiche(Player myPlayer) {
+        ArrayList<Integer> listHost = new ArrayList<Integer>();
+        ArrayList<Integer> listFriend = new ArrayList<Integer>();
+        ArrayList<Integer> listChest = new ArrayList<Integer>();
         mPlayer=myPlayer;
         listNPC=myPlayer.getActualRoom().getNPCRoom();
-        //final ImageIcon backgroundImage = new ImageIcon("C:/imgWoZ/ground.jpg");
-        //JLabel gameGrid = new JLabel(backgroundImage) {
-        //};
+        listCh=myPlayer.getActualRoom().getChestRoom();
+        nbPos=(myPlayer.getActualRoom().getNPCRoom().size())+(myPlayer.getActualRoom().getChestRoom().size());
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
-
+        //----------------------------------------------------------------------
+        ArrayList<Integer> listPos = new ArrayList<Integer>();
+        listPos.add(22);
+        listPos.add(2);
+        listPos.add(10);
+        listPos.add(14);
         //----------------------------------------------------------------------
         //-----------------------------GAME GRID--------------------------------
         gameGrid = new JPanel();
@@ -73,99 +90,119 @@ private ArrayList<NPC> listNPC;
         }        
 
         //----------------------------------------------------------------------
+        for(int cpt=0; cpt<listNPC.size();cpt++){
+            rdVal=ThreadLocalRandom.current().nextInt(min, max + 1);
+            while(listPos.contains(rdVal)){
+                    rdVal=ThreadLocalRandom.current().nextInt(min, max + 1);
+            }
+            if(listNPC.get(cpt).isHostile()){                   
+                listHost.add(rdVal);    
+            }
+            else{
+                listFriend.add(rdVal);
+            }
+        }
+        for(int cpt=0; cpt<listCh.size();cpt++){
+            rdVal=ThreadLocalRandom.current().nextInt(min, max + 1);
+            while(listPos.contains(rdVal)){
+                    rdVal=ThreadLocalRandom.current().nextInt(min, max + 1);
+            }
+            listChest.add(rdVal);
+        }
+        //----------------------------------------------------------------------
         arrayButton = new JButton[myPlayer.getActualRoom().getNPCRoom().size()];
         arrayChest = new JButton[myPlayer.getActualRoom().getChestRoom().size()];
-        for(int j=0; j< myPlayer.getActualRoom().getNPCRoom().size(); j++){
-            System.out.println("NPC"+j);           
-        }
-        //------------------------Adding NPC------------------------------------
- 
-        //--------------------
-        for(int row=0; row < 5; row ++){
-            for(int col=0; col < 5; col ++){
+        //---------------------------
+        for(int nbButt=0; nbButt <25; nbButt++){
         //----------------------------------------------------------------------
         //-----------------------------Adding Doors-----------------------------
-                if(south==true && row==4 && col==2)
+                if(south==true && nbButt==22)
                 {
-                    ImageIcon porte = new ImageIcon(new ImageIcon("C:/imgWoz/porte1.jpg").getImage().getScaledInstance(120,160, Image.SCALE_DEFAULT));
+                    ImageIcon porte = new ImageIcon(new ImageIcon("C:/imgWoz/dungeonDoor.png").getImage().getScaledInstance(190,190, Image.SCALE_DEFAULT));
                     doorSouth = new JButton(porte);
                     doorSouth.setPreferredSize(new Dimension(100,100));
                     doorSouth.setMinimumSize(new Dimension(100,100));
                     gameGrid.add(doorSouth);
                     myPlayer.setActualRoom(roomHM.get("South").getNextRoom());
-                    doorSouth.addActionListener(this);
-                    System.out.println(this.getComponentZOrder(doorSouth));
-                    
-                    
+                    doorSouth.addActionListener(this);                   
                 }
-                else if(north==true && row==0 && col==2)
+                else if(north==true && nbButt==2)
                 {
-                    ImageIcon porte = new ImageIcon(new ImageIcon("C:/imgWoz/porte1.jpg").getImage().getScaledInstance(120,160, Image.SCALE_DEFAULT));
-                    JButton button = new JButton(porte);
-                    button.setPreferredSize(new Dimension(100,100));
-                    button.setMinimumSize(new Dimension(100,100));
-                    gameGrid.add(button);
+                    ImageIcon porte = new ImageIcon(new ImageIcon("C:/imgWoz/dungeonDoor.png").getImage().getScaledInstance(190,190, Image.SCALE_DEFAULT));
+                    JButton doorNorth = new JButton(porte);
+                    doorNorth.setPreferredSize(new Dimension(100,100));
+                    doorNorth.setMinimumSize(new Dimension(100,100));
+                    gameGrid.add(doorNorth);
+                    myPlayer.setActualRoom(roomHM.get("North").getNextRoom());
+                    doorNorth.addActionListener(this);
                 }
-                else if(west==true && row==2 && col==0)
+                else if(west==true && nbButt==10)
                 {
-                    ImageIcon porte = new ImageIcon(new ImageIcon("C:/imgWoz/porte1.jpg").getImage().getScaledInstance(120,160, Image.SCALE_DEFAULT));
-                    JButton button = new JButton(porte);
-                    button.setPreferredSize(new Dimension(100,100));
-                    button.setMinimumSize(new Dimension(100,100));
-                    gameGrid.add(button);
-                    //System.out.println(doorWest.getX()+" "+doorWest.getY());
+                    ImageIcon porte = new ImageIcon(new ImageIcon("C:/imgWoz/dungeonDoor.png").getImage().getScaledInstance(190,190, Image.SCALE_DEFAULT));
+                    JButton doorWest = new JButton(porte);
+                    doorWest.setPreferredSize(new Dimension(100,100));
+                    doorWest.setMinimumSize(new Dimension(100,100));
+                    gameGrid.add(doorWest);
+                    myPlayer.setActualRoom(roomHM.get("West").getNextRoom());
+                    doorWest.addActionListener(this);
                 }
-                else if(east==true && row==2 && col==4)
+                else if(east==true && nbButt==14)
                 {
-                    ImageIcon porte = new ImageIcon(new ImageIcon("C:/imgWoz/porte1.jpg").getImage().getScaledInstance(120,160, Image.SCALE_DEFAULT));
-                    JButton button = new JButton(porte);
-                    button.setPreferredSize(new Dimension(100,100));
-                    button.setMinimumSize(new Dimension(100,100));
-                    gameGrid.add(button);
+                    ImageIcon porte = new ImageIcon(new ImageIcon("C:/imgWoz/dungeonDoor.png").getImage().getScaledInstance(190,190, Image.SCALE_DEFAULT));
+                    JButton doorEast = new JButton(porte);
+                    doorEast.setPreferredSize(new Dimension(100,100));
+                    doorEast.setMinimumSize(new Dimension(100,100));
+                    gameGrid.add(doorEast);
+                    myPlayer.setActualRoom(roomHM.get("East").getNextRoom());
+                    doorEast.addActionListener(this);
                 }
-                else if( i < arrayButton.length && nbButt!=2 && nbButt!=10 && nbButt!=14 && nbButt!=22 ){
-                    if(listNPC.get(i).isHostile()){
-                        ImageIcon host = new ImageIcon(new ImageIcon("C:/imgWoz/monstre1.png").getImage().getScaledInstance(120,160, Image.SCALE_DEFAULT));
-                        arrayButton[i]= new JButton(host);
-                        arrayButton[i].setPreferredSize(new Dimension(100,100));
-                        arrayButton[i].setMinimumSize(new Dimension(100,100));
-                        gameGrid.add(arrayButton[i]);
-                        arrayButton[i].addActionListener(this);
-                        i++;
+                
+        //----------------------------------------------------------------------
+
+                
+        //-----------------------------Adding NPC-------------------------------
+                else if(listHost.contains(nbButt)){
+                        ImageIcon host = new ImageIcon(new ImageIcon("C:/imgWoz/monstre1.png").getImage().getScaledInstance(190,190, Image.SCALE_DEFAULT));
+                        JButton btHost= new JButton(host);
+                        btHost.setPreferredSize(new Dimension(100,100));
+                        btHost.setMinimumSize(new Dimension(100,100));
+                        gameGrid.add(btHost);
+                        btHost.addActionListener(this);
                     }
-                    else{
-                        ImageIcon friendly = new ImageIcon(new ImageIcon("C:/imgWoz/friendly.jpg").getImage().getScaledInstance(120,160, Image.SCALE_DEFAULT));
-                        arrayButton[i] = new JButton(friendly);
-                        arrayButton[i].setPreferredSize(new Dimension(100,100));
-                        arrayButton[i].setMinimumSize(new Dimension(100,100));
-                        gameGrid.add(arrayButton[i]);
-                        arrayButton[i].addActionListener(this);
-                        i++;
+        //----------------------------------------------------------------------
+        //-----------------------------Adding Friendly--------------------------            
+                else if(listFriend.contains(nbButt)){
+                        ImageIcon friendly = new ImageIcon(new ImageIcon("C:/imgWoz/dungeonDwarf.png").getImage().getScaledInstance(190,190, Image.SCALE_DEFAULT));
+                        JButton btFriend= new JButton(friendly);
+                        btFriend.setPreferredSize(new Dimension(100,100));
+                        btFriend.setMinimumSize(new Dimension(100,100));
+                        gameGrid.add(btFriend);
+                        btFriend.addActionListener(this);
                     }
+        //----------------------------------------------------------------------
+        //-----------------------------Adding Chests----------------------------        
+                else if(listChest.contains(nbButt)){
+                    ImageIcon chest= new ImageIcon(new ImageIcon("C:/imgWoz/dungeonChest.png").getImage().getScaledInstance(190,190, Image.SCALE_DEFAULT));
+                    JButton btChest= new JButton(chest);
+                    btChest.setPreferredSize(new Dimension(100,100));
+                    btChest.setMinimumSize(new Dimension(100,100));
+                    gameGrid.add(btChest);
+                    btChest.addActionListener(this);
                 }
-                else if(c < arrayChest.length && nbButt!=2 && nbButt!=10 && nbButt!=14 && nbButt!=22){
-                    ImageIcon chest= new ImageIcon(new ImageIcon("C:/imgWoz/chest.png").getImage().getScaledInstance(120,160, Image.SCALE_DEFAULT));
-                    arrayChest[c]= new JButton(chest);
-                    arrayChest[c].setPreferredSize(new Dimension(100,100));
-                    arrayChest[c].setMinimumSize(new Dimension(100,100));
-                    gameGrid.add(arrayChest[c]);
-                    arrayChest[c].addActionListener(this);
-                    c++;
-                }
-                else{
-                    
-                    ImageIcon dalle1 = new ImageIcon(new ImageIcon("C:/imgWoz/dalleFinal.png").getImage().getScaledInstance(120,160, Image.SCALE_DEFAULT));
+        //----------------------------------------------------------------------
+        //-----------------------------Adding Floor-----------------------------        
+                else{   
+                    ImageIcon dalle1 = new ImageIcon(new ImageIcon("C:/imgWoz/dalleFinal.png").getImage().getScaledInstance(190,190, Image.SCALE_DEFAULT));
                     JButton button2 = new JButton(dalle1);
                     button2.setPreferredSize(new Dimension(100,100));
                     button2.setMinimumSize(new Dimension(100,100));
                     gameGrid.add(button2);
                 }
-                nbButt++;
             }
-            
-        }
-        gameGrid.setPreferredSize(new Dimension(600,500));//largeur,hauteur
-        gameGrid.setMinimumSize(new Dimension(600,500));
+                
+        
+        gameGrid.setPreferredSize(new Dimension(900,900));//largeur,hauteur
+        gameGrid.setMinimumSize(new Dimension(900,900));
         this.add(gameGrid,BorderLayout.WEST);
         this.pack();
         this.setVisible(true);
@@ -175,12 +212,28 @@ private ArrayList<NPC> listNPC;
     //----------------------ActionListener---------------------------
     public void actionPerformed (ActionEvent e){
          if (e.getSource().equals(doorSouth)){
-            System.out.println("toto");
             Graphique newAffiche = new Graphique();
             gameGrid=newAffiche.Affiche(mPlayer);
             this.dispose();
             this.setContentPane(newAffiche.Affiche(mPlayer));
-                     
+         }
+         else if (e.getSource().equals(doorNorth)){
+            Graphique newAffiche = new Graphique();
+            gameGrid=newAffiche.Affiche(mPlayer);
+            this.dispose();
+            this.setContentPane(newAffiche.Affiche(mPlayer));     
+        }
+        else if (e.getSource().equals(doorEast)){
+            Graphique newAffiche = new Graphique();
+            gameGrid=newAffiche.Affiche(mPlayer);
+            this.dispose();
+            this.setContentPane(newAffiche.Affiche(mPlayer));     
+        }
+        else if (e.getSource().equals(doorWest)){
+            Graphique newAffiche = new Graphique();
+            gameGrid=newAffiche.Affiche(mPlayer);
+            this.dispose();
+            this.setContentPane(newAffiche.Affiche(mPlayer));     
         }
 
        
